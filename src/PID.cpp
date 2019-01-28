4e0e2041fa20e08b2f33bdf7d75cb68a1817a0ae
Ragupathy Gopalakrishnan <13126445+ragu-git@users.noreply.github.com>
2019-01-27 22:26:10 -0800

    Updated PID Control logic

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,11 @@
+#include <stdlib.h>
 #include "PID.h"
+
+#define WINDOW 20
+
+
+using namespace std;
+
 
 /**
  * TODO: Complete the PID class. You may add any additional desired functions.
@@ -8,23 +15,33 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp_, double Ki_, double Kd_) {
-  /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
-   */
-
+void PID::Init(double Kp, double Ki, double Kd) {
+	this->Kp = Kp;
+	this->Ki = Ki;
+	this->Kd = Kd;
+	p_error = 0;
+	i_error = 0;
+	d_error = 0;
+	window      = (int*) calloc( WINDOW , sizeof(window[0]) ) ;
+	i           = WINDOW - 1 ;
+	sum         = 0 ;
 }
 
-void PID::UpdateError(double cte) {
-  /**
-   * TODO: Update PID errors based on cte.
-   */
-
+void PID::UpdateError(double cte, double dt) {
+	d_error = (cte - p_error) / dt;
+	p_error = cte;
+	i_error = sum_i(cte * dt);
 }
 
-double PID::TotalError() {
-  /**
-   * TODO: Calculate and return the total error
-   */
-  return 0.0;  // TODO: Add your total error calc here!
+double PID::TotalError(double cur_vel) {
+	  //PID parameters corrected based on the vehicle velocity.
+	return (Kp - 0.0032 * cur_vel) * p_error + Ki * i_error + (Kd + 0.0002 * cur_vel) * d_error;
+}
+
+// For i_error calculation we sum up only last 20 measurements
+// // inspired by code from https://stackoverflow.com/questions/25024012/running-sum-of-the-last-n-integers-in-an-array
+double PID::sum_i(double err){
+	i          = (i+1) % WINDOW ;
+	sum        = sum - window[i] + err ;
+  return sum;
 }
